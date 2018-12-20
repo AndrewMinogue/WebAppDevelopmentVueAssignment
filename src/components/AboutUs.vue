b0abf933e2a84130cfa157f44b08a4ca729afda2
AndrewMinogue <a.minogue@hotmail.com>
2018-12-20 21:38:29 +0000

    Implemented Youtube Video with API and Google Maps

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hero">
     <h3 class="vue-title"><i class="fa fa-book small" style="padding: 4px"></i>About Us</h3>
+    <p style="color:black"> We are an Irish company that prides ourselves on our homegrown ingredients</p><br/>
+    <p style="color:black"> The Company was created in 2018 by a man named Andrew Minogue</p><br/>
+    <p style="color:black"> We Created this video to show you how to make the perfect pizza</p>
+    <p> </p>
+    <iframe width="700" height="500" src="https://www.youtube.com/embed/YQkC0Qs3aD0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 </template>
 
@@ -34,11 +39,5 @@ export default {
     text-align: center;
     font-size: 40pt;
     margin-bottom: 10px;
-  }
-  .video{
-    margin-top:80px;
-    align-content: center;
-    width:300px;
-    height:300px;
   }
 </style>
--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -4,6 +4,10 @@
     <h2 class="vue-title1">Waterford Store only</h2>
     <p>email :  pizzaparty@gmail.com</p>
     <p>Phone Number :  1800123456</p>
+    <div class="mapouter">
+      <div class="gmap_canvas">
+      <iframe width="100%" height="500" style="align-content: center" id="gmap_canvas" src="https://maps.google.com/maps?q=Ireland%2C%20Waterford&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+      </iframe><a href="https://www.crocothemes.net"></a></div></div>
   </div>
 </template>
 
@@ -19,6 +23,13 @@
 </script>
 
 <style scoped>
+  .hero {
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #000;
+  }
   .vue-title {
     margin-top: 30px;
     text-align: center;
@@ -28,11 +39,23 @@
   .vue-title1 {
     margin-top: 30px;
     text-align: center;
-    font-size: 35pt;
+    font-size: 30pt;
     margin-bottom: 10px;
   }
-  .hero{
-    font-weight: 100;
-    font-size: 20pt;
+  .mapouter{
+    text-align:center;
+    align-content: center;
+    float:right;
+    height:500px;
+    width:600px;
+  }
+  .gmap_canvas {
+    overflow:hidden;
+    background:none!important;
+    text-align:center;
+    align-content: center;
+    justify-content: center;
+    height:500px;
+    width:100%;
   }
 </style>
